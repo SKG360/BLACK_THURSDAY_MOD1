584ac374355ff61f096d3e113de573da8e6cb0d2
SKG360 <samuel.giles@colorado.edu>
2018-09-11 17:01:20 -0600

    Completes Sales Engine

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,5 +1,15 @@
 class SalesEngine
-  def self.from_csv
+  attr_reader :items, :merchants
+  
+  def initialize(items, merchants)
+    @items = items
+    @merchants = merchants
+  end
 
-  end 
+  def self.from_csv(hash_arg)
+    items = ItemRepository.new(hash_arg[:items])
+    merchants = MerchantRepository.new(hash_arg[:merchants])
+    SalesEngine.new(items, merchants)
+  end
+
 end
---
+++ test/sales_engine_test.rb
@@ -0,0 +1,39 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'bigdecimal'
+require 'time'
+require './lib/item'
+require './lib/item_repository'
+require './lib/merchant'
+require './lib/merchant_repository'
+require './lib/sales_engine'
+
+class SalesEngineTest < Minitest::Test
+  def test_if_it_exists
+    se = SalesEngine.from_csv({
+    :items     => "./data/items.csv",
+    :merchants => "./data/merchants.csv",
+    })
+    assert_instance_of SalesEngine, se
+  end
+
+  def test_it_returns_an_instance_of_merchant_repo
+    se = SalesEngine.from_csv({
+    :items     => "./data/items.csv",
+    :merchants => "./data/merchants.csv",
+    })
+
+    mr = se.merchants
+    assert_instance_of MerchantRepository, mr
+  end
+
+  def test_it_returns_an_instance_of_item_repo
+    se = SalesEngine.from_csv({
+    :items     => "./data/items.csv",
+    :merchants => "./data/merchants.csv",
+    })
+
+    ir = se.items
+    assert_instance_of ItemRepository, ir
+  end
+end
