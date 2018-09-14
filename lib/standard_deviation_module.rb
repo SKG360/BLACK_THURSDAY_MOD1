b47a0162fad9d39a47fbc9159d13d6327b2fff88
Jenn Lao <jenn.p.lao@gmail.com>
2018-09-14 01:59:39 -0600

    adds standard deviation module and fixes golden items

--- lib/standard_deviation_module.rb
+++ lib/standard_deviation_module.rb
@@ -1,6 +1,30 @@
 module StandardDeviation
-  
-  def mean
 
+  def difference_from_mean(average, collection)
+    collection.map do |value|
+      (average - value).round(2)
+    end
+  end
+
+  def differences_squared(average, collection)
+    dfm = difference_from_mean(average, collection)
+    dfm.map do |difference|
+      (difference ** 2).round(2)
+    end
+  end
+
+  def sum_of_squared_differences(average, collection)
+    ds = differences_squared(average, collection)
+    diff_aggregator = 0
+    ds.each do |diff|
+      diff_aggregator += diff
+    end
+    diff_aggregator.round(2)
+  end
+
+  def divided_sum(average, collection)
+    dfm = difference_from_mean(average, collection)
+    ssd = sum_of_squared_differences(average, collection)
+    (ssd / (dfm.length - 1)).round(2)
   end
 end
