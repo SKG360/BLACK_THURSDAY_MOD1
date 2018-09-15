19ab21f46b579fdbcda1cdc95514dd90299a5c68
SKG360 <samuel.giles@colorado.edu>
2018-09-15 13:53:20 -0600

    Adds invoice item repo test & methods

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,20 +1,22 @@
 require 'time'
+require 'bigdecimal'
 
 class InvoiceItem
   attr_reader :id,
               :item_id,
               :invoice_id,
-              :quantity,
-              :unit_price,
-              :created_at,
-              :updated_at
+              :created_at
+
+  attr_accessor :quantity,
+                :unit_price,
+                :updated_at
 
   def initialize(data)
     @id = data[:id].to_i
     @item_id = data[:item_id].to_i
     @invoice_id = data[:invoice_id].to_i
     @quantity = data[:quantity].to_i
-    @unit_price = data[:unit_price]
+    @unit_price = BigDecimal.new(data[:unit_price].to_f / 100, 5)
     @created_at  = Time.parse(data[:created_at].to_s)
     @updated_at  = Time.parse(data[:updated_at].to_s)
   end
--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -23,4 +23,21 @@ class InvoiceItemRepository
       @storage << InvoiceItem.new(row)
     end
   end
+
+  def find_all_by_item_id(item_id)
+    @storage.find_all do |item|
+      item.item_id == item_id
+    end
+  end
+
+  def update(id, attributes)
+  invoice = find_by_id(id)
+  no_quantity = attributes[:quantity].nil?
+  no_unit_price = attributes[:unit_price].nil?
+
+  invoice.quantity = attributes[:quantity] unless no_quantity
+  invoice.unit_price = attributes[:unit_price] unless no_unit_price
+  invoice.updated_at = Time.now unless no_quantity && no_unit_price
+  invoice
+  end
 end
