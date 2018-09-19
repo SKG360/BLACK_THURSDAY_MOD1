a442251c6645ce7b52da9d609b8ee6b0a460efea
Jenn Lao <jenn.p.lao@gmail.com>
2018-09-18 21:29:21 -0600

    fixes spacing for customer repo test

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -11,12 +11,12 @@ require './lib/sales_engine'
 class CustomerRepositoryTest < Minitest::Test
   def test_if_it_exists
     se = SalesEngine.from_csv({
-    :items     => "./data/items.csv",
-    :merchants => "./data/merchants.csv",
-    :invoices => "./data/invoices.csv",
-    :invoice_items => "./data/invoice_items.csv",
-    :transactions => "./data/transactions.csv",
-    :customers => "./data/customers.csv"
+      items: './data/items.csv',
+      merchants: './data/merchants.csv',
+      invoices: './data/invoices.csv',
+      invoice_items: './data/invoice_items.csv',
+      transactions: './data/transactions.csv',
+      customers: './data/customers.csv'
     })
 
     assert_instance_of CustomerRepository, se.customers
@@ -24,12 +24,12 @@ class CustomerRepositoryTest < Minitest::Test
 
   def test_it_return_all_customers
     se = SalesEngine.from_csv({
-    :items     => "./data/items.csv",
-    :merchants => "./data/merchants.csv",
-    :invoices => "./data/invoices.csv",
-    :invoice_items => "./data/invoice_items.csv",
-    :transactions => "./data/transactions.csv",
-    :customers => "./data/customers.csv"
+      items: './data/items.csv',
+      merchants: './data/merchants.csv',
+      invoices: './data/invoices.csv',
+      invoice_items: './data/invoice_items.csv',
+      transactions: './data/transactions.csv',
+      customers: './data/customers.csv'
     })
 
     customer = se.customers
@@ -38,12 +38,12 @@ class CustomerRepositoryTest < Minitest::Test
 
   def test_it_can_find_customers_by_id
     se = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
-      :invoices => "./data/invoices.csv",
-      :invoice_items => "./data/invoice_items.csv",
-      :transactions => "./data/transactions.csv",
-      :customers => "./data/customers.csv"
+      items: './data/items.csv',
+      merchants: './data/merchants.csv',
+      invoices: './data/invoices.csv',
+      invoice_items: './data/invoice_items.csv',
+      transactions: './data/transactions.csv',
+      customers: './data/customers.csv'
     })
 
     customer = se.customers
@@ -55,102 +55,104 @@ class CustomerRepositoryTest < Minitest::Test
 
   def test_it_can_find_customers_by_first_name
     se = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
-      :invoices => "./data/invoices.csv",
-      :invoice_items => "./data/invoice_items.csv",
-      :transactions => "./data/transactions.csv",
-      :customers => "./data/customers.csv"
+      items: './data/items.csv',
+      merchants: './data/merchants.csv',
+      invoices: './data/invoices.csv',
+      invoice_items: './data/invoice_items.csv',
+      transactions: './data/transactions.csv',
+      customers: './data/customers.csv'
     })
 
     customer = se.customers
-    assert_equal 8, customer.find_all_by_first_name("oe").length
-    assert_equal 57, customer.find_all_by_first_name("NN").length
-    assert_instance_of Customer, customer.find_all_by_first_name("oe")[0]
+    assert_equal 8, customer.find_all_by_first_name('oe').length
+    assert_equal 57, customer.find_all_by_first_name('NN').length
+    assert_instance_of Customer, customer.find_all_by_first_name('oe')[0]
   end
 
   def test_it_can_find_customers_by_last_name
     se = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
-      :invoices => "./data/invoices.csv",
-      :invoice_items => "./data/invoice_items.csv",
-      :transactions => "./data/transactions.csv",
-      :customers => "./data/customers.csv"
+      items: './data/items.csv',
+      merchants: './data/merchants.csv',
+      invoices: './data/invoices.csv',
+      invoice_items: './data/invoice_items.csv',
+      transactions: './data/transactions.csv',
+      customers: './data/customers.csv'
     })
 
     customer = se.customers
-    assert_equal 85, customer.find_all_by_last_name("On").length
-    assert_equal 85, customer.find_all_by_last_name("oN").length
-    assert_instance_of Customer, customer.find_all_by_last_name("On")[0]
+    assert_equal 85, customer.find_all_by_last_name('On').length
+    assert_equal 85, customer.find_all_by_last_name('oN').length
+    assert_instance_of Customer, customer.find_all_by_last_name('On')[0]
   end
 
   def test_it_can_create_new_customer
     se = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
-      :invoices => "./data/invoices.csv",
-      :invoice_items => "./data/invoice_items.csv",
-      :transactions => "./data/transactions.csv",
-      :customers => "./data/customers.csv"
+      items: './data/items.csv',
+      merchants: './data/merchants.csv',
+      invoices: './data/invoices.csv',
+      invoice_items: './data/invoice_items.csv',
+      transactions: './data/transactions.csv',
+      customers: './data/customers.csv'
     })
 
-    customer = se.customers
-
     attributes = {
-      :first_name => "Joan",
-      :last_name => "Clarke",
-      :created_at => Time.now,
-      :updated_at => Time.now
+      first_name: 'Joan',
+      last_name: 'Clarke',
+      created_at: Time.now,
+      updated_at: Time.now
     }
+
+    customer = se.customers
     customer.create(attributes)
     assert_equal 1001, customer.storage[-1].id
-    assert_equal "Joan", customer.storage[-1].first_name
+    assert_equal 'Joan', customer.storage[-1].first_name
   end
 
   def test_it_can_update_customer
     se = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
-      :invoices => "./data/invoices.csv",
-      :invoice_items => "./data/invoice_items.csv",
-      :transactions => "./data/transactions.csv",
-      :customers => "./data/customers.csv"
+      items: './data/items.csv',
+      merchants: './data/merchants.csv',
+      invoices: './data/invoices.csv',
+      invoice_items: './data/invoice_items.csv',
+      transactions: './data/transactions.csv',
+      customers: './data/customers.csv'
     })
 
     attributes = {
-      :first_name => "Joan",
-      :last_name => "Clarke",
-      :created_at => Time.now,
-      :updated_at => Time.now
+      first_name: 'Joan',
+      last_name: 'Clarke',
+      created_at: Time.now,
+      updated_at: Time.now
     }
+
     customer = se.customers
     customer.create(attributes)
 
-    attributes_2 = {last_name: "Smith", id: 987800}
+    attributes_2 = {last_name: 'Smith', id: 987800}
 
     customer.update(1001, attributes_2)
     assert_equal 1001, customer.storage[-1].id
-    assert_equal "Smith", customer.storage[-1].last_name
+    assert_equal 'Smith', customer.storage[-1].last_name
     refute_equal customer.storage[-1].updated_at, customer.storage[-1].created_at
   end
 
   def test_it_can_delete_customer
     se = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
-      :invoices => "./data/invoices.csv",
-      :invoice_items => "./data/invoice_items.csv",
-      :transactions => "./data/transactions.csv",
-      :customers => "./data/customers.csv"
+      items: './data/items.csv',
+      merchants: './data/merchants.csv',
+      invoices: './data/invoices.csv',
+      invoice_items: './data/invoice_items.csv',
+      transactions: './data/transactions.csv',
+      customers: './data/customers.csv'
     })
 
     attributes = {
-      :first_name => "Joan",
-      :last_name => "Clarke",
-      :created_at => Time.now,
-      :updated_at => Time.now
+      first_name: 'Joan',
+      last_name: 'Clarke',
+      created_at: Time.now,
+      updated_at: Time.now
     }
+
     customer = se.customers
     customer.create(attributes)
     customer.delete(1001)
