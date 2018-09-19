574b70ca025809c80b3e8035673f682906f54492
Jenn Lao <jenn.p.lao@gmail.com>
2018-09-18 22:00:21 -0600

    fixes customer and invoice items tests

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -9,28 +9,28 @@ require './lib/invoice_item'
 class InvoiceItemTest < Minitest::Test
   def test_it_exists
     ii = InvoiceItem.new({
-      :id => 6,
-      :item_id => 7,
-      :invoice_id => 8,
-      :quantity => 1,
-      :unit_price => BigDecimal.new(10.99, 4),
-      :created_at => Time.now,
-      :updated_at => Time.now
-      })
+      id: 6,
+      item_id: 7,
+      invoice_id: 8,
+      quantity: 1,
+      unit_price: BigDecimal.new(10.99, 4),
+      created_at: Time.now,
+      updated_at: Time.now
+    })
 
     assert_instance_of InvoiceItem, ii
   end
 
   def test_the_attributes
     ii = InvoiceItem.new({
-      :id => 6,
-      :item_id => 7,
-      :invoice_id => 8,
-      :quantity => 1,
-      :unit_price => BigDecimal.new(10.99, 4),
-      :created_at => Time.now,
-      :updated_at => Time.now
-      })
+      id: 6,
+      item_id: 7,
+      invoice_id: 8,
+      quantity: 1,
+      unit_price: BigDecimal.new(10.99, 4),
+      created_at: Time.now,
+      updated_at: Time.now
+    })
 
     assert_equal 6, ii.id
     assert_equal 7, ii.item_id
@@ -40,29 +40,29 @@ class InvoiceItemTest < Minitest::Test
 
   def test_that_the_unit_price_is_big_decimal
     ii = InvoiceItem.new({
-      :id => 6,
-      :item_id => 7,
-      :invoice_id => 8,
-      :quantity => 1,
-      :unit_price => BigDecimal.new(10.99, 4),
-      :created_at => Time.now,
-      :updated_at => Time.now
-      })
+      id: 6,
+      item_id: 7,
+      invoice_id: 8,
+      quantity: 1,
+      unit_price: BigDecimal.new(109.9, 4),
+      created_at: Time.now,
+      updated_at: Time.now
+    })
 
-    assert_equal 10.99, ii.unit_price
+    assert_equal 1.099, ii.unit_price
     assert_instance_of BigDecimal, ii.unit_price
   end
 
   def test_the_create_updated_time_stamps
     ii = InvoiceItem.new({
-      :id => 6,
-      :item_id => 7,
-      :invoice_id => 8,
-      :quantity => 1,
-      :unit_price => BigDecimal.new(10.99, 4),
-      :created_at => Time.now,
-      :updated_at => Time.now
-      })
+      id: 6,
+      item_id: 7,
+      invoice_id: 8,
+      quantity: 1,
+      unit_price: BigDecimal.new(10.99, 4),
+      created_at: Time.now,
+      updated_at: Time.now
+    })
 
     assert_instance_of Time, ii.created_at
     assert_instance_of Time, ii.updated_at
@@ -70,16 +70,15 @@ class InvoiceItemTest < Minitest::Test
 
   def test_the_unit_price_conversion_to_float
     ii = InvoiceItem.new({
-      :id => 6,
-      :item_id => 7,
-      :invoice_id => 8,
-      :quantity => 1,
-      :unit_price => BigDecimal.new(10.99, 4),
-      :created_at => Time.now,
-      :updated_at => Time.now
-      })
+      id: 6,
+      item_id: 7,
+      invoice_id: 8,
+      quantity: 1,
+      unit_price: BigDecimal.new(10.99, 4),
+      created_at: Time.now,
+      updated_at: Time.now
+    })
 
-    assert_equal 10.99, ii.unit_price_to_dollars
+    assert_equal 0.11, ii.unit_price_to_dollars
   end
-
 end
